dfdd7355ccf976708703db5590cc9af01bfd7006
Eliandro Rodrigues <eliandrorodrigues17@gmail.com>
2020-05-22 23:41:49 -0300

    alteração 1

--- calculadora_simples/script.js
+++ calculadora_simples/script.js
@@ -1,8 +1,14 @@
-function botao(num){
-    let salvo = document.calc.visor.value
-    document.calc.visor.value = salvo + num
+function botao(valor){
+    document.calc.visor.value += valor
 }
 
 function limpar(){
     document.getElementById('visor').value = ''
+}
+
+function calcular(){
+    let resultado
+    resultado = document.getElementById('visor').value
+    document.getElementById('visor').value = ''
+    document.getElementById('visor').value = eval(resultado)
 }
